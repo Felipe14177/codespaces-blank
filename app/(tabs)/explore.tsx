94828e0d4a4e09e1b4d84af4f715a408293a1efd
Felipe14177 <141411365+Felipe14177@users.noreply.github.com>
2024-06-18 12:42:06 +0000

    first commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,8 +1,5 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
+import { StyleSheet, Image, View, Pressable, Linking } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -11,92 +8,104 @@ export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      headerImage={
+        <Image source={require('@/assets/images/logo.jpg')} style={styles.headerImage} />
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Redes sociais</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+
+      <ThemedView style={styles.contentContainer}>
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/1.png')} style={styles.icon} />
+          <Pressable  onPress={() => Linking.openURL('https://wa.me/qr/5D2XL6JUTF74P1')}>
+            <ThemedText>Whatsapp</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/7.png')} style={styles.icon} />
+          <Pressable  onPress={() => Linking.openURL('https://www.linkedin.com/in/douglas-eduardo-a26327312/')}>
+            <ThemedText>Linkedin</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/8.png')} style={styles.icon} />
+          <Pressable  onPress={() => Linking.openURL('mailto:douglasclassroomeduardo@gmail.com')}>
+            <ThemedText>Email</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/2.png')} style={styles.icon} />
+          <Pressable onPress={() => Linking.openURL('https://github.com/Duduardoaajkk')}>
+            <ThemedText>Github</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/3.png')} style={styles.icon} />
+          <Pressable  onPress={() => Linking.openURL('https://br.pinterest.com/douglasclassroomeduardo/')}>
+            <ThemedText>Pinterest</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/4.png')} style={styles.icon} />
+          <Pressable onPress={() => Linking.openURL('tel:5518998106469')}>
+            <ThemedText>Telefone</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/5.png')} style={styles.icon} />
+          <Pressable  onPress={() => Linking.openURL('https://www.instagram.com/duduardoajk/')}>
+            <ThemedText>Instagram</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+        <ThemedView style={styles.redes}>
+          <Image source={require('@/assets/images/6.png')} style={styles.icon} />
+          <Pressable  onPress={() => Linking.openURL('https://x.com/douglsapenas')}>
+            <ThemedText>Twiter</ThemedText>
+          </Pressable>
+        </ThemedView>
+
+      </ThemedView>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
   titleContainer: {
+    padding: 16,
+  },
+  contentContainer: {
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  redes: {
     flexDirection: 'row',
-    gap: 8,
+     
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 16,
+    backgroundColor: 'white',
+    borderRadius: 100,
+    padding: 10
+  },
+  icon: {
+    width: 40,
+    height: 40,
+    marginRight: 8,
+  },
+  text: {
+    fontSize: 16,
   },
 });
